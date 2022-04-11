90a0a4f0ac0ac0586b8f3368fa6004c35580a2de
Cory Levsen <corylevsen@hotmail.com>
2022-04-11 16:42:25 -0600

    Buy now api not working

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,26 @@
 
 import {PageTitle} from "../components/PageTitle"
-
+import Head from 'next/head'
 import ProductCard from "../components/ProductCard/ProductCard"
 
 // https://assignments-dee2d-default-rtdb.firebaseio.com/products.json
 
 
+
 export default function Home(props) {
-  const products = props.products
+  const products = props.products.slice(0,3)
+
+  console.log(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
+  console.log(process.env.STRIPE_SECRET_KEY)
+
   return (
     <>
+    <Head>
+      <meta charset="UTF-8"/>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      <title>Storefront</title>
+    </Head>
       <PageTitle title="Storefront" tagline="featured products"/>
       <main>
         {
@@ -28,6 +39,8 @@ export async function getStaticProps() {
   return {
     props:{
       products
-    }
+    },
+    revalidate: 60
   }
 }
+
